0f2c0ff6460de0dcfd16e90d55c370907919839e
Arthur AMORIM <arthur.amorim@universite-paris-saclay.fr>
2025-09-15 15:19:35 +0200

    Improved proposed read_string and cleanup

--- src/MAT_v5.jl
+++ src/MAT_v5.jl
@@ -262,108 +262,63 @@ end
 
 truncate_to_uint8(x) = x % UInt8
 
+function MAT_uint16_to_char(c_u16::UInt16)
+    char = convert(Char, c_u16)
+    if 255 < convert(UInt32, char)
+        # Newer versions of MATLAB seem to write some mongrel UTF-8...
+        char = String([truncate_to_uint8(c_u16>>8), truncate_to_uint8(c_u16)])[1]
+    end
+    char
+end
+
 function read_string(f::IO, swap_bytes::Bool, dimensions::Vector{Int32})
     (dtype, nbytes, hbytes) = read_header(f, swap_bytes)
     read_type = READ_TYPES[dtype]
     if sizeof(read_type)*prod(dimensions) != nbytes
         error("Invalid element length")
     end
-    # We will assume that the last dimension corresponds the String direction
-    # This behavior is consistent with matlab for ndim<=2, but not for ndim>2
+    # We will assume that the last dimension corresponds the String direction.
+    # This behavior is consistent with certain third-party libraries like SciPy.io.
+    # This behavior is somewhat consistent with MATLAB for ndim<=2, although MATLAB
+    # appears to interpret char arrays as if the chars are splitted (not strings)
+    # i.e. in MATLAB, `['ab' 'ef' ; 'hi' 'kl'] == ['a' 'b' 'e' 'f' ; 'h' 'i' 'k' 'l']`
+    # but this array will be printed `2×4 char ['abef' ; 'hikl']`.
     ndim = length(dimensions)
+    dim_tuple = tuple(convert(Vector{Int}, dimensions)...)
+    CI_slices = CartesianIndices(dim_tuple[1:ndim-1]))
     if dtype <= 2 || dtype == miUTF8
         # If dtype <= 2, this may give an error on non-ASCII characters, since the string
         # would be ISO-8859-1 and not UTF-8. However, MATLAB 2012b always saves strings with
         # a 2-byte encoding in v6 format, and saves UTF-8 in v7 format. Thus, this may never
         # happen in the wild.
-        chars = read!(f, Array{UInt8}(undef, tuple(convert(Vector{Int}, dimensions)...)))
-        data = String[rstrip(String(s)) for s in eachslice(chars; dims=tuple((1:ndim-1)...))]
-        # Version above is somehow more efficient than
+        chars = read!(f, Array{UInt8}(undef, dim_tuple))
+        data = map(ci->String(view(chars, ci, :)), CI_slices)
+        # Alternative below requires VERSION >= v"1.9" due to `eachslice`
+        # `data = String[rstrip(String(s)) for s in eachslice(chars; dims=tuple((1:ndim-1)...))]`
+        # Both are much more efficient than
         # `data = String.(dropdims(mapslices(rstrip∘String, chars; dims=ndim); dims=ndim))`
     elseif dtype <= 4 || dtype == miUTF16
         # Technically, if dtype == 3 or dtype == 4, this is ISO-8859-1 and not Unicode.
         # However, the first 256 Unicode code points are derived from ISO-8859-1, so UCS-2
         # is a superset of 2-byte ISO-8859-1.
-        chars = read_bswap(f, swap_bytes, UInt16, tuple(convert(Vector{Int}, dimensions)...))
-        CIs = CartesianIndices(tuple(convert(Vector{Int}, dimensions[1:ndim-1])...))
-        bufs = [IOBuffer() for ci in CIs]
-        length_slice = ndim == 0 ? 1 : dimensions[ndim]
-        for ci in CIs
-            for j in 1:length_slice
-                char = convert(Char, chars[ci, j])
-                if 255 < convert(UInt32, char)
-                    # Newer versions of MATLAB seem to write some mongrel UTF-8...
-                    char = String([truncate_to_uint8(chars[ci, j] >> 8), truncate_to_uint8(chars[ci, j])])[1]
-                end
-                write(bufs[ci], char)
-            end
-        end
-        data = String[rstrip(String(take!(buf))) for buf in bufs]
+        chars = read_bswap(f, swap_bytes, UInt16, dim_tuple)
+        length_slice = ndim==0 ? 1 : dimensions[ndim]
+        buf = Vector{Char}(undef, length_slice)
+        data = map(ci -> String(map!(special_uint16_to_char, buf, view(chars, ci, :))), CI_slices)
     elseif dtype == miUTF32
-        chars = read_bswap(f, swap_bytes, UInt32, tuple(convert(Vector{Int}, dimensions)...))
-        char_buffer = Vector{Char}(undef, length_slice)
-        data = String[rstrip(String(copyto!(char_buffer, s))) for s in eachslice(chars; dims=tuple((1:ndim-1)...))]
-        # Version above is as expected more efficient than
-        # `data = String[(rstrip∘String)(Char.(s)) for s in eachslice(chars; dims=tuple((1:ndim-1)...))]`
-        # which is somehow more efficient than
-        # `data = String.(dropdims(mapslices(rstrip∘String, convert(Array{Char}, chars); dims=ndim); dims=ndim))`
+        chars = read_bswap(f, swap_bytes, UInt32, dim_tuple)
+        length_slice = ndim==0 ? 1 : dimensions[ndim]
+        buf = Vector{Char}(undef, length_slice)
+        data = map(ci -> String(copyto!(buf, view(chars, ci, :))), CI_slices)
     else
         error("Unsupported string type")
     end
+    map!(String∘rstrip, data, data)
     if any(dimensions == 0) || length(data) == 0
         data = ""
     elseif ndims(data) == 0
         # no returning 0-dimensionnal arrays
         data = data[1]
-    end
-    skip_padding(f, nbytes, hbytes)
-    data
-end
-
-function read_string_old(f::IO, swap_bytes::Bool, dimensions::Vector{Int32})
-    (dtype, nbytes, hbytes) = read_header(f, swap_bytes)
-    if dtype <= 2 || dtype == 16
-        # If dtype <= 2, this may give an error on non-ASCII characters, since the string
-        # would be ISO-8859-1 and not UTF-8. However, MATLAB 2012b always saves strings with
-        # a 2-byte encoding in v6 format, and saves UTF-8 in v7 format. Thus, this may never
-        # happen in the wild.
-        chars = read!(f, Vector{UInt8}(undef, nbytes))
-        if dimensions[1] <= 1
-            data = String(chars)
-        else
-            data = Vector{String}(undef, dimensions[1])
-            for i = 1:dimensions[1]
-                data[i] = rstrip(String(chars[i:dimensions[1]:end]))
-            end
-        end
-    elseif dtype <= 4 || dtype == 17
-        # Technically, if dtype == 3 or dtype == 4, this is ISO-8859-1 and not Unicode.
-        # However, the first 256 Unicode code points are derived from ISO-8859-1, so UCS-2
-        # is a superset of 2-byte ISO-8859-1.
-        chars = read_bswap(f, swap_bytes, UInt16, convert(Int, div(nbytes, 2)))
-        bufs = [IOBuffer() for i = 1:dimensions[1]]
-        i = 1
-        while i <= length(chars)
-            for j = 1:dimensions[1]
-                char = convert(Char, chars[i])
-                if 255 < convert(UInt32, char)
-                    # Newer versions of MATLAB seem to write some mongrel UTF-8...
-                    char = String([truncate_to_uint8(chars[i] >> 8), truncate_to_uint8(chars[i])])[1]
-                end
-                write(bufs[j], char)
-                i += 1
-            end
-        end
-
-        if dimensions[1] == 0
-            data = ""
-        elseif dimensions[1] == 1
-            data = String(take!(bufs[1]))
-        else
-            data = String[rstrip(String(take!(buf))) for buf in bufs]
-        end
-    else
-        error("Unsupported string type")
     end
     skip_padding(f, nbytes, hbytes)
     data
